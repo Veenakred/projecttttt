ded88b09dcdcdbb08fd43a36927cddbac2afc708
Veenakred <99552080+Veenakred@users.noreply.github.com>
2022-11-27 13:27:38 +0000

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,10 @@
+function addUser () {
+  
+  player1_name_input = player1_name_input;
+  player2_name_input = player2_name_input;
+
+ localStorage((player1_name_input)(player2_name_input));
+
+  window.location(game_page.html);
+}
+
